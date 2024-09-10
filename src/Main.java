dbc221fb3c12c7e99386cad17ef9b82a9fd4bb53
USER <mas9n9-mas9n91332@yandex.ru>
2024-09-10 17:44:53 +0500

    hm3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//                                         tesk 1
+        int a = 150;
+        byte b = 4;
+        short c = 100;
+        long d = 2000L;
+        float f = 12.5f;
+        double g = 4.1234567890;
+        System.out.println("Значение переменной "+"int "+"с типом "+"Целочисленные "+"равно "+a);
+        System.out.println("Значение переменной "+"byte "+"с типом "+"Целочисленные "+"равно "+b);
+        System.out.println("Значение переменной "+"short "+"с типом "+"Целочисленные "+"равно "+c);
+        System.out.println("Значение переменной "+"long "+"с типом "+"Целочисленные "+"равно "+d);
+        System.out.println("Значение переменной "+"float "+"с типом "+"С плавающей точкой "+"равно "+f);
+        System.out.println("Значение переменной "+"double "+"с типом "+"С плавающей точкой "+"равно " +g);
+        //                                 tesk 2
+        float aa = 27.12f;
+        long ab = 987678965549L;
+        float ac =2.786f;
+        int ad = 569;
+        short aaa = -159;
+        short aab = 27897;
+        byte aac = 67;
     }
-
 }
 
